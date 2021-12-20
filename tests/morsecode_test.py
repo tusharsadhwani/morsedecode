059b1db4f3167cd623fe2600095b1c8964ae7919
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-12-21 00:10:22 +0530

    Add some more tests

--- tests/morsecode_test.py
+++ tests/morsecode_test.py
@@ -3,14 +3,26 @@ import pytest
 import morsecode
 
 
-@pytest.mark.parametrize(("string", "output"), (("SOS", "... --- ..."),))
+@pytest.mark.parametrize(
+    ("string", "output"),
+    (
+        ("SOS", "... --- ..."),
+        ("OH, HI MARK!", "--- .... --..-- / .... .. / -- .- .-. -.- -.-.--"),
+    ),
+)
 def test_encode(string, output):
     """Tests encoding from text to morse code"""
 
     assert morsecode.encode(string) == output
 
 
-@pytest.mark.parametrize(("string", "output"), (("... --- ...", "SOS"),))
+@pytest.mark.parametrize(
+    ("string", "output"),
+    (
+        ("... --- ...", "SOS"),
+        ("--- .... --..-- / .... .. / -- .- .-. -.- -.-.--", "OH, HI MARK!"),
+    ),
+)
 def test_decode(string, output):
     """Tests encoding from text to morse code"""
 
